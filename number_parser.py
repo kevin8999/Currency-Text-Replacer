b7d03a3d40804c9bc7c9123c82666b5e1ac40e49
Kevin D <76125913+Vomet@users.noreply.github.com>
2025-01-04 16:49:39 -0500

    Add number parser

---
+++ number_parser.py
@@ -0,0 +1,62 @@
+"""
+Finds numbers in string format and converts them to integer.
+
+Valid numbers include:
+
+- 1,234,567.89
+- 1.234.567,89
+- 1 234 567,89
+"""
+
+class NumberParser:
+    def __init__(self, number: str) -> None:
+        # self.number_str will be converted to self.number in NumberParser
+        self.number_str = number
+        self.number = None
+
+        # Create list of strings from 0 to 9
+        self.VALID_DIGITS = [str(i) for i in range(10)]
+
+        self.decimal_separator_pos = -1
+        self.thousands_separator_pos = []
+        self.SEPARATORS = [",", ".", "_", " ", "'"]
+        self.DECIMAL_SEPARATORS = [",", "."]
+
+    def find(self):
+        separators_found = []
+
+        # Find separators and check if digit is a valid digit
+        for digit in self.number_str:
+            if digit in self.SEPARATORS:
+                separators_found.append(digit)
+                continue
+            if digit not in self.VALID_DIGITS:
+                raise ValueError(f"{digit} is not a number.")
+
+        num_separators_found = len(set(separators_found))
+
+        # If EXACTLY two unique separators are found, this means there is a thousands separator and a decimal separator
+        if num_separators_found == 1:
+            thousands_separator = separators_found[0]
+            self.number = self.number_str.replace(thousands_separator, "")
+        elif num_separators_found == 2:
+            decimal_separator = separators_found[-1]
+            thousands_separator = separators_found[0]
+            if decimal_separator in self.DECIMAL_SEPARATORS:
+                whole_number, decimal = self.number_str.split(decimal_separator)
+
+                whole_number = whole_number.replace(thousands_separator, "")
+
+                # Combine whole_number and decimal separated by "." to turn into float
+                self.number = whole_number + "." + decimal
+                self.number = float(self.number)
+            else:
+                raise ValueError(f"{decimal_separator} is not a valid decimal separator.")
+
+        elif num_separators_found > 2:
+            raise ValueError("Separators found is greater than 2. Not a valid number.")
+
+num = "1_234789.99956"
+num_parser = NumberParser(num)
+num_parser.find_separators()
+print(num_parser.number)
