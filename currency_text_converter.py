0b75f7a78f60f727c0455bc006eeb3867a03a894
Kevin D <76125913+Vomet@users.noreply.github.com>
2025-01-03 22:33:33 -0500

    Convert to class
    TODO: Fix infinite loop issue on line 89

--- currency_text_converter.py
+++ currency_text_converter.py
@@ -4,164 +4,163 @@ import re
 #import requests
 
 def find_pattern(text, pattern):
-    # Returns all instances of `pattern` in `text`
+    # Returns all instances of `pattern` in `self.text`
     regex = rf"{pattern}"
     finder = re.compile(regex)
-    matches = finder.findall(text)
+    matches = finder.findall(self.text)
     print(matches)
     return matches
 
-def find_number(move_backwards: bool, symbol_index, text, condition):
-    # Finds number in text from currency_symbol
-    i = symbol_index
+class Currency:
+    def __init__(self, currency_code, currency_data, text):
+        self.currency_code = currency_code
+        self.currency_data = currency_data
 
-    num = []
-    while True:
-        if move_backwards:
-            char = text[i-1]
-        else:
-            char = text[i+1]
+        # Get name and plural name of currency
+        name = currency_data["name"].replace(currency_data["demonym"], "") # Remove demonym from currency
+        plural_name = name.replace(currency_data["majorSingle"], currency_data["majorPlural"])
+        name = name.strip().upper()
+        plural_name = plural_name.strip().upper()
 
-        try:
-            int(char)
-        except:
-            # If the first character before the currency symbol is a space and spaces are allowed or permitted, move onto the next character
-            if (condition["spaces_allowed"] == "required" or condition["spaces_allowed"] == "optional") and \
-                abs(symbol_index - i) == 1 and \
-                char == " ":
-                    if move_backwards:
-                        i -= 1
-                    else:
-                        i += 1
+        self.numbers = []
+        self.text = text
+        self.THOUSANDS_SEPARATORS = [",", ".", " ", "_"]
+
+        """
+        `self.symbol_condition` states when a currency symbol can be used in English.
+
+        It contains 3 keys:
+
+        - placed_before: currency symbol may be placed before a quantity.
+        - placed_after: currency symbol may be placed after a quantity.
+        - spaces_allowed: currency symbol may have a space between it and the amount.
+
+        NOTE: for `currency_data['symbol']` and `currency_data['symbolNative']`,
+        some currencies only allow you to place a symbol before or after the amount.
+        However, this common grammatical mistake will be automatically converted.
+        """
+
+        self.symbol_condition = {
+            currency_code: {
+                'placed_before': True,
+                'placed_after': True,
+                'spaces_allowed': "optional"
+            },
+            currency_data['symbol']: {
+                'placed_before': True,
+                'placed_after': True,
+                'spaces_allowed': "required"
+            },
+            currency_data['symbolNative']: {
+                'placed_before': True,
+                'placed_after': True,
+                'spaces_allowed': "forbidden"          
+            },
+            currency_data['name'].upper(): {
+                'placed_before': False,
+                'placed_after': True,
+                'spaces_allowed': "optional"
+            },
+            name: {
+                'placed_before': False,
+                'placed_after': True,
+                'spaces_allowed': "required"
+            },
+            plural_name: {
+                'placed_before': False,
+                'placed_after': True,
+                'spaces_allowed': "required"
+            }
+        }
+
+    def find_number(self, move_backwards: bool, symbol_index, condition):
+        # Finds number in self.text from currency_symbol
+        i = symbol_index
+
+        num = []
+        while True:
+            if move_backwards:
+                char = self.text[i-1]
+            else:
+                char = self.text[i+1]
+
+            try:
+                int(char)
+            except:
+                print(f"{symbol_index} - {i}")
+                # If the first character before the currency symbol is a space and spaces are allowed or permitted, move onto the next character
+                if (condition["spaces_allowed"] == "required" or condition["spaces_allowed"] == "optional") and \
+                    abs(symbol_index - i) == 1 and \
+                    char == " ":
+                        if move_backwards:
+                            i -= 1
+                        else:
+                            i += 1
+                        continue
+                elif condition["spaces_allowed"] == "forbidden" and \
+                    abs(symbol_index - i) == 1 and \
+                    char == " ":
+                        break
+
+                if char in self.THOUSANDS_SEPARATORS:
+                    num.append(char)
+                    i -= 1
                     continue
-            elif condition["spaces_allowed"] == "forbidden" and \
-                abs(symbol_index - i) == 1 and \
-                char == " ":
-                    break
 
-            THOUSANDS_SEPARATORS = [",", ".", " ", "_"]
-            if char in THOUSANDS_SEPARATORS:
+                break
+            else:
                 num.append(char)
                 i -= 1
-                continue
 
-            break
-        else:
-            num.append(char)
-            i -= 1
+        # Reverse num and return
+        num = num[::-1]
+        num = ''.join(num)
+        num = num.strip()
+        return num
 
-    # Reverse num and return
-    num = num[::-1]
-    num = ''.join(num)
-    num = num.strip()
-    return num
+    def find_currency_symbol(self, symbol, condition):
+        # Look for symbol in self.text
 
-def find_currency_symbol(text, symbol, condition):
-    # Look for symbol in text
+        """
+        Matches currency symbol with numbers
 
-    """
-    Matches currency symbol with numbers
+        Examples:
 
-    Examples:
+        - $1,000.00
+        - 234.000.000,00 USD
+        - USD 10000.00
+        """
 
-    - $1,000.00
-    - 234.000.000,00 USD
-    - USD 10000.00
-    """
+        # Stores indices of where the currency index occurs
+        currency_indices = []
+        matches = re.finditer(symbol, self.text)
+        currency_indices = [(match.start(), match.end()) for match in matches]
+        print(currency_indices)
 
-    # Stores indices of where the currency index occurs
-    currency_indices = []
-    matches = re.finditer(symbol, text)
-    currency_indices = [(match.start(), match.end()) for match in matches]
-    print(currency_indices)
+        # Find numbers next to currency_indices
+        for start, end in currency_indices:
+            if condition['placed_before'] == True:
+                num = self.find_number(move_backwards=True, symbol_index=start, condition=condition)
+                self.numbers.append(num)
 
-    # Find numbers next to currency_indices
-    numbers = []
-    for start, end in currency_indices:
-        if condition['placed_before'] == True:
-            num = find_number(move_backwards=True, symbol_index=start, text=text, condition=condition)
-            numbers.append(num)
-            """i = start
-            while True:
-                char = text[i-1]
-                try:
-                    int(char)
-                except:
-                    # If the first character before the currency symbol is a space and spaces are allowed or permitted, move onto the next character
-                    if (condition["spaces_allowed"] == "required" or condition["spaces_allowed"] == "optional") and \
-                        i == start-1 and \
-                        char == " ":
-                            i -= 1
-                            continue
-                    elif condition["spaces_allowed"] == "forbidden" and \
-                        i == start-1 and \
-                        char == " ":
-                            break
+            if condition['placed_after'] == True:
+                i = end
+                num = self.find_number(move_backwards=False, symbol_index=end, condition=condition)
 
-                    THOUSANDS_SEPARATORS = [",", ".", " ", "_"]
-                    if char in THOUSANDS_SEPARATORS:
-                        num.append(char)
-                        i -= 1
-                        continue
+        print(self.numbers)
 
-                    break
-                else:
-                    num.append(char)
-                    i -= 1"""
+    def find(self):
+        results = {}
+        for key in self.symbol_condition.keys():
+            if key == '$':
+                # If the symbol is a "$", escape it so that regex works
+                results[key] = self.find_currency_symbol(r"\$", self.symbol_condition[key]) 
+            else:
+                results[key] = self.find_currency_symbol(key, self.symbol_condition[key])
+        from pprint import pprint
+        pprint(results)
 
-            print(numbers)
 
-        if condition['placed_after'] == True:
-            i = end
-            num = find_number(move_backwards=False, symbol_index=end, text=text, condition=condition)
-
-            """while True:
-
-                
-                char = text[i+1]
-                try:
-                    int(char)
-                except:
-                    # If the first character before the currency symbol is a space and spaces are allowed or permitted, move onto the next character
-                    if (condition["spaces_allowed"] == "required" or condition["spaces_allowed"] == "optional") and \
-                        i == end+1 and \
-                        char == " ":
-                            i += 1
-                            continue
-                    elif condition["spaces_allowed"] == "forbidden" and \
-                        i == end+1 and \
-                        char == " ":
-                            break
-
-                    THOUSANDS_SEPARATORS = [",", ".", " ", "_"]
-                    if char in THOUSANDS_SEPARATORS:
-                        num.append(char)
-                        i += 1
-                        continue
-
-                    break
-                else:
-                    num.append(char)
-                    i -= 1"""
-
-        """decimal_separator = None
-        if "," in word or "." in word:
-            # Determine if "," or "." occurs last to find decimal separator
-
-            # Get all indexes of commas and periods and find the index where each occurs
-            commas = [index for index, char in enumerate(text) if char == ","]
-            periods = [index for index, char in enumerate(text) if char == "."]
-            last_comma = max(commas)
-            last_period = max(periods)
-            
-            if last_comma > last_period:
-                decimal_separator = ","
-            elif last_period > last_comma:
-                decimal_separator = "."
-        else:
-            number = int(word)"""
-
-    print(numbers)
 
 def convert_currency(amount, currency_from, currency_to):
     if currency_from == currency_to:
@@ -174,70 +173,9 @@ def main(text, currency_code):
         data = json.load(file)
 
     currency_data = data[currency_code]
+    converter = Currency(currency_code, currency_data, text)
+    converter.find()
 
-    # Generate all possible currency symbols
-    name = currency_data["name"].replace(currency_data["demonym"], "") # Remove demonym from currency
-    plural_name = name.replace(currency_data["majorSingle"], currency_data["majorPlural"])
-    name = name.strip().upper()
-    plural_name = plural_name.strip().upper()
-
-    """
-    `symbol_condition` states when a currency symbol can be used in English.
-
-    It contains 3 keys:
-
-    - placed_before: currency symbol may be placed before a quantity.
-    - placed_after: currency symbol may be placed after a quantity.
-    - spaces_allowed: currency symbol may have a space between it and the amount.
-
-    NOTE: for `currency_data['symbol']` and `currency_data['symbolNative']`,
-    some currencies only allow you to place a symbol before or after the amount.
-    However, this common grammatical mistake will be automatically converted.
-    """
-
-    symbol_condition = {
-        currency_code: {
-            'placed_before': True,
-            'placed_after': True,
-            'spaces_allowed': "optional"
-        },
-        currency_data['symbol']: {
-            'placed_before': True,
-            'placed_after': True,
-            'spaces_allowed': "required"
-        },
-        currency_data['symbolNative']: {
-            'placed_before': True,
-            'placed_after': True,
-            'spaces_allowed': "forbidden"          
-        },
-        currency_data['name'].upper(): {
-            'placed_before': False,
-            'placed_after': True,
-            'spaces_allowed': "optional"
-        },
-        name: {
-            'placed_before': False,
-            'placed_after': True,
-            'spaces_allowed': "required"
-        },
-        plural_name: {
-            'placed_before': False,
-            'placed_after': True,
-            'spaces_allowed': "required"
-        }
-    }
-
-    results = {}
-
-    for key in symbol_condition.keys():
-        if key == '$':
-            # If the symbol is a "$", escape it so that regex works
-            results[key] = find_currency_symbol(text, r"\$", symbol_condition[key]) 
-        else:
-            results[key] = find_currency_symbol(text, key, symbol_condition[key])
-    from pprint import pprint
-    pprint(results)
 
 
 if __name__ == '__main__':
@@ -261,7 +199,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args.currency = args.currency.upper()
 
-    # Check if user enters BOTH args.text and args.file or NEITHER args.text nor args.file
+    # Raise error if user enters BOTH args.text and args.file or NEITHER args.text nor args.file
     if args.text != None and args.file != None:
         raise ValueError("Cannot enter text and file at the same time. Please split your command into two separate commands.")
     elif args.text == None and args.file == None:
