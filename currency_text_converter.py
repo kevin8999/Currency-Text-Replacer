8e4ba6673ca2e8059e3c85b113353357309f959c
Kevin D <76125913+Vomet@users.noreply.github.com>
2025-01-05 12:38:35 -0500

    Add working converter and require two currencies as input for currency_text_converter.py

--- currency_text_converter.py
+++ currency_text_converter.py
@@ -3,6 +3,8 @@ import argparse
 import re
 from number_parser import NumberParser
 from price_parser import PriceParser
+from update_exchange_rates import ExchangeRates
+import subprocess
 
 
 class CurrencyConverter:
@@ -29,13 +31,29 @@ class CurrencyConverter:
         return amount_to
         
 
-def main(text, currency_code):
+def main(text, currency_from, currency_to):
     with open('currencies.json') as file:
         data = json.load(file)
 
-    currency_data = data[currency_code]
-    price_parser = PriceParser(currency_code, currency_data, text)
+    # Find all prices in text that use currency_from
+    currency_data = data[currency_from]
+    price_parser = PriceParser(currency_from, currency_data, text)
     price_parser.find()
+    print(price_parser.results)
+
+    # Get current exchange rates
+    print("Updating exchange rates...")
+    update_rates_cmd = "python update_exchange_rates.py".split()
+    subprocess.run(update_rates_cmd)
+
+    # Convert currencies
+    currency_converter = CurrencyConverter()
+    converted_values = {}
+    for key in price_parser.results:
+        converted_values[key] = []
+        for result in price_parser.results[key]:
+            converted = currency_converter.convert(result, currency_from, currency_to)
+            converted_values[key].append(converted)
 
 
 if __name__ == '__main__':
@@ -52,12 +70,16 @@ if __name__ == '__main__':
                         required=False,
                         default='output.txt',
                         help='Name of output file. Default: output.txt')
-    parser.add_argument('-c', '--currency',
+    parser.add_argument('-a', '--currency_from',
                         required=True,
-                        help='Name of currency you would like to convert to. This program uses the ISO 4217 standard.')
+                        help='Currency you would like to convert from. Currency must conform to ISO 4217 standard.')
+    parser.add_argument('-b', '--currency_to',
+                        required=True,
+                        help='Currency you would like to convert to. Currency must conform to ISO 4217 standard.')
 
     args = parser.parse_args()
-    args.currency = args.currency.upper()
+    args.currency_from = args.currency_from.upper()
+    args.currency_to = args.currency_to.upper()
 
     # Raise error if user enters BOTH args.text and args.file or NEITHER args.text nor args.file
     if args.text != None and args.file != None:
@@ -76,7 +98,9 @@ if __name__ == '__main__':
     # Check if currency is in valid_currencies.txt
     with open('valid_currencies.txt') as file:
         valid_currencies = file.read().splitlines() 
-    if args.currency not in valid_currencies:
-        raise ValueError(f"{args.currency} was not found in valid_currencies.txt.")
+    if args.currency_from not in valid_currencies:
+        raise ValueError(f"{args.currency_from} was not found in valid_currencies.txt.")
+    if args.currency_to not in valid_currencies:
+        raise ValueError(f"{args.currency_to} was not found in valid_currencies.txt.")
 
-    main(text, args.currency)
+    main(text, args.currency_from, args.currency_to)
