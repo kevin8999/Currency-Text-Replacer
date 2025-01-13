c76c3f18d6050600f7909c0317ae58379b3a35e0
Kevin D <76125913+Vomet@users.noreply.github.com>
2025-01-13 14:04:44 -0500

    Add code that respects original number format
    For example, if you have this number:
    
    200,000.00
    
    It should output
    
    288,328.81
    
    or something similar after conversion
    
    Note that integers will be rounded to the hundredths after conversion.
    
    BROKEN
    
    Numbers such as 3,000 will output 4000 (no thousands separator or decimal separator at all)

--- currency_text_converter.py
+++ currency_text_converter.py
@@ -26,7 +26,8 @@ class CurrencyConverter:
                 raise KeyError(f"{currency} not found in {self.exchange_rate_file}.")
 
         # Convert amount_from to USD
-        USD_amount = amount_from / self.exchange_rates[currency_from]
+        USD_rate = self.exchange_rates[currency_from]
+        USD_amount = amount_from['value']['number'] / USD_rate
 
         # Convert USD_amount to target currency
         amount_to = USD_amount * self.exchange_rates[currency_to]
@@ -53,41 +54,90 @@ def main(text, currency_from, currency_to, output_file):
 
     # Convert currencies
     currency_converter = CurrencyConverter()
-    converted_values = {}
+    converted_values = []
     print(f"Converting currencies from {currency_from} to {currency_to}...")
-    for key in price_parser.results:
-        converted_values[key] = []
-        for i, result in enumerate(price_parser.results[key]):
-            converted = currency_converter.convert(result, currency_from, currency_to)
+    for result in price_parser.prices:
+        converted = currency_converter.convert(result, currency_from, currency_to)
 
-            # Round based on number of decimal places in original number
-            try:
-                num = price_parser.separator_positions[key][i]['number']
-                num_decimal_places = len(num)-1 - price_parser.separator_positions[key][i]['decimal_sep_pos'][1]
-            except:
-                num_decimal_places = 0
+        # Round based on number of decimal places in original number
+        try:
+            decimal_pos = result['value']['separator_positions']['decimal'][1]
+            num_decimal_places = decimal_pos-1
+        except:
+            num_decimal_places = 0
 
-            rounded = round(converted, num_decimal_places)
+        # Convert to price notation (with 2 decimal places), unless num_decimal_places equals 0
+        price = Decimal(str(converted)).quantize(Decimal(".01"), rounding=ROUND_DOWN)
 
-            # Convert to price notation, unless num_decimal_places equals 0
-            price = Decimal(str(rounded)).quantize(Decimal(".01"), rounding=ROUND_DOWN)
+        price = str(price)
 
-            if num_decimal_places == 0:
-                price = str(int(price))
+        # Add thousands separators if present in original string
+        thousands_separators = result['value']['separator_positions']['thousands']
+        try:
+            sep_pos = [pos for (symbol, pos) in thousands_separators]
+            # Only one type of thousands separator is used, so grabbing only the first one will give you
+            # the thousands separator for the number
+            sep_symbol = thousands_separators[0][0]
+            decimal_separator = result['value']['separator_positions']['decimal'][0]
+        except TypeError:
+            # Raises error if decimal_separator = None
+            decimal_separator = None
+        except:
+            pass
+        else:
+            # Split price at decimal separator if it exists
+            price = price.split(decimal_separator)
+            whole_num = list(price[0])
+
+            # Number of digits per thousands group
+            digits_in_thousands_group = None
+            if result['value']['uses_indian_thousands_system']:
+                # Add 1 to account for list index
+                digits_in_thousands_group = 2
+
+                # Insert first thousands separator
+                whole_num.insert(-3, sep_symbol)
+
+                # Insert all other thousands separators
+                i = len(whole_num) - 6
+                while i > 0:
+                    whole_num.insert(i, sep_symbol)
+                    i -= digits_in_thousands_group
             else:
-                price = str(price)
+                # Add 1 to account for list index
+                digits_in_thousands_group = 3 + 1
 
-            converted_values[key].append(price)
+                # Insert first thousands separator
+                # If length % 3 = 0, set it to 3 because numbers do not begin with a thousands separator
+                first_thous_pos = 3 if len(whole_num) % 3 == 0 else len(whole_num) % 3
+                whole_num.insert(first_thous_pos, sep_symbol)
+
+                # Insert all other thousands separators
+                i = first_thous_pos + digits_in_thousands_group
+                while i < len(whole_num):
+                    whole_num.insert(i, sep_symbol)
+                    i += digits_in_thousands_group
+                
+            whole_num = ''.join(whole_num)
+            price[0] = whole_num
+
+            price = f'{decimal_separator}'.join(price)
+
+        converted_values.append(price)
     
     print(f"\tPrices converted to {currency_to}.")
-    print(price_parser.found)
-    print(converted_values)
-
-    # Convert prices to string
-
+    
+    print("Original\tNew")
+    for i, value in enumerate(converted_values):
+        print(f"{price_parser.prices[i]['amount']}\t\t{value}")
 
     # Write to file
     print(f"Writing to changes to {output_file}...")
+
+    """
+    with open(output_file, "w") as file:
+        file.write(new_text)
+    """
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Convert text from currency A to currency B using current exchange rates.")
